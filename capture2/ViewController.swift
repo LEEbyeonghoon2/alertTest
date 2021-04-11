b495294e71d805846d937df837e871b821273a11
lee <dog10041@naver.com>
2021-04-11 14:59:15 +0900

    Initial Commit

---
+++ capture2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  capture2
+//
+//  Created by 이병훈 on 2021/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
